a340a2a56c09f0ddfd1a89b7473ded99fcbacb80
Carlos Toledo <carlombo@hotmail.com>
2022-10-12 16:25:25 -0300

    Se realiza migración, pero se tuvieron que borrar los productos antes, xq no aceptaba productos con user null

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_12_184542) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_12_191532) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,7 +20,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_12_184542) do
     t.bigint "brand_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
     t.index ["brand_id"], name: "index_articles_on_brand_id"
+    t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
   create_table "brands", force: :cascade do |t|
@@ -42,4 +44,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_12_184542) do
   end
 
   add_foreign_key "articles", "brands"
+  add_foreign_key "articles", "users"
 end
