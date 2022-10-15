f41494d07b0896e73896e6f65a27e5ada0ce2ed9
Carlos Toledo <carlombo@hotmail.com>
2022-10-14 22:24:03 -0300

    Se configuran los roles para que, al crear un nuevo un nuevo usuario, se cree como vendedor. También, se colocó la condición que solo el admin pueda editar o borrar marcas

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+
+  protected
+  def tengo_permisos?
+    if !current_user.admin?
+      redirect_to brands_path, notice: "No tienes permisos para esta acción"
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
         :recoverable, :rememberable, :validatable
+
+  enum rol: [:vendedor, :admin]
+
+  after_initialize do
+    if self.new_record?
+      self.rol ||= :vendedor
+    end
+  end
+
 end
