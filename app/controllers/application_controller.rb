5aee99065a02eceecdf60b04161dc41cef244492
Carlos Toledo <carlombo@hotmail.com>
2022-10-15 00:14:42 -0300

    Se agrega condición para que no puedan editar los artíuclos los que no esten logueados

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,4 +9,11 @@ class ApplicationController < ActionController::Base
       redirect_to brands_path, notice: "No tienes permisos para esta acción"
     end
   end  
+
+  def permisos?
+    if !user_signed_in?
+      redirect_to articles_path, notice: "No tienes permisos para esta acción"
+    end
+  end
+
 end
