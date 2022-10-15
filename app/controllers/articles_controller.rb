b63ac07676c8941c2acce7c6b5143576bdd8fe8a
Carlos Toledo <carlombo@hotmail.com>
2022-10-14 21:27:35 -0300

    Se agregró formulario para capturar el user en nuevo artículo y se modificó el parámetro. Se agregó en layout un mostrador de uusuario

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -66,6 +66,6 @@ class ArticlesController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def article_params
-      params.require(:article).permit(:name, :detail, :brand_id)
+      params.require(:article).permit(:name, :detail, :brand_id, :user_id)
     end
 end
