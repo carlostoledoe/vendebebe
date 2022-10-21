c1379fb87d9a8168c9bd561d55aed6f476aa6fe3
Carlos Toledo <carlombo@hotmail.com>
2022-10-21 18:17:14 -0300

    Corrección errores de sintaxis en los modelos

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,5 @@
 class Order < ApplicationRecord
   belongs_to :user
-  has_many :order_details, dependent: destroy
-  has_many :articles, through :order_details
+  has_many :order_details, dependent: :destroy
+  has_many :articles, through: :order_details
 end
